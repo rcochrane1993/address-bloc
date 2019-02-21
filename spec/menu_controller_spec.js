a6ff2e10096d645069e97bc12d5c5d1a8bd171fd
Robert Cochrane <rcochrane1105@gmail.com>
2019-02-21 11:11:58 -0500

    testing checkpoint done

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -15,10 +15,4 @@ describe("MenuController", () => {
             expect(this.menu.getContactCount()).toBe(1)
           });
     });
-
-    describe("#remindMe()", () => {
-        it("should return 'Learning is a life-long pursuit'"), () => {
-            expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit")
-        }
-    })
 });
