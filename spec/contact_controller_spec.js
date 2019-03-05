6017d63f7bd9dfe0c7b2dde5823f78931fa95f95
rcochrane1993 <35496351+rcochrane1993@users.noreply.github.com>
2019-03-05 13:05:43 -0500

    contacts progress

---
+++ spec/contact_controller_spec.js
@@ -0,0 +1,23 @@
+const sequelize = require("../db/models/index").sequelize;
+
+describe("ContactController", () => {
+  beforeEach((done) => {
+    this.book = new ContactController();
+
+    sequelize.sync({force: true}).then((res) => {
+      done();
+    })
+    .catch((err) => {
+      done();
+    });
+  });
+  describe("#addContact()",() => {
+
+    it("should add a single contact into the book", () => {
+      expect(this.book.contacts.length).toBe(0);
+      this.book.addContact("Alice", "001-101-1010");
+      expect(this.book.contacts.length).toBe(1);
+    });
+  });
+  
+});
